0c831485b617abea6119a2cc2369872a3cfe4d7d
Antoine MARTIN <amartin@aldebaran.com>
2017-02-22 13:47:23 +0100

    version 0.2
    Change-Id: I827a6517a51e998b93b0f1f8fb87ff6cfe66c74c
    Reviewed-on: http://gerrit.aldebaran.lan/80272
    Reviewed-by: amartin <amartin@aldebaran.com>
    Tested-by: amartin <amartin@aldebaran.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 # For development purposes, install it with pip install -user -e
 # or python setup.py develop
 setup(
-    version="0.1",
+    version="0.2",
     name="wooqi",
     description="Pytest plugin allowing to parametrize all the test sequence thanks to a config file",
     packages=find_packages(),
